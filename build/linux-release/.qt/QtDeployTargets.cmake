1955ad838da423703159f70087c673b3d0d62ec5
Sivn <sivn@foxmail.com>
2025-04-15 18:38:39 +0800

    Remove build N add ignore

--- build/linux-release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_TicketManageSystem_FILE /home/sivn/Repos/TicketManageSystem/build/linux-release/TicketManageSystem)
-set(__QT_DEPLOY_TARGET_TicketManageSystem_TYPE EXECUTABLE)
